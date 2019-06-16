dc6a6749e37b3d33359bdd3137bfd02a435a168d
GorelkinIA <48888453+GorelkinIA@users.noreply.github.com>
2019-06-16 13:18:44 +0300

    Доделано создание и модификация групп с учетом сравнивания списка

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,31 +3,31 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name);
+
+
+
+  public int getId() {
+    return id;
   }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(name);
-  }
-
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "name='" + name + '\'' +
-            '}';
+  public void setId(int id) {
+    this.id = id;
   }
 
   public GroupData(String name, String header, String footer) {
+    this.id = 0;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public GroupData(int id, String name, String header, String footer) {
+    this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -44,4 +44,28 @@ public class GroupData {
   public String getFooter() {
     return footer;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return id == groupData.id &&
+            Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name);
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            '}';
+  }
+
 }
+
