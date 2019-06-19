fefe33d57fbe9b8b7fd752487837559c3dff0866
GorelkinIA <48888453+GorelkinIA@users.noreply.github.com>
2019-06-19 21:51:39 +0300

    Реализована проверка при удалении контакта. Действительно ли тот удалился?

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -9,27 +9,6 @@ public class ContactData {
   private final String mail;
   private String group;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    ContactData that = (ContactData) o;
-    return Objects.equals(name, that.name) &&
-            Objects.equals(firstname, that.firstname);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name, firstname);
-  }
-
-  @Override
-  public String toString() {
-    return "ContactData{" +
-            "name='" + name + '\'' +
-            ", firstname='" + firstname + '\'' +
-            '}';
-  }
 
   public ContactData(String name, String firstname, String mobileTelephone, String mail, String group) {
     this.name = name;
@@ -57,5 +36,27 @@ public class ContactData {
 
   public String getGroup() {
     return group;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ContactData that = (ContactData) o;
+    return Objects.equals(name, that.name) &&
+            Objects.equals(firstname, that.firstname);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, firstname);
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "name='" + name + '\'' +
+            ", firstname='" + firstname + '\'' +
+            '}';
   }
 }
