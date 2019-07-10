32072207aff65780d9e6edb978d639053a794dd5
GorelkinIA <48888453+GorelkinIA@users.noreply.github.com>
2019-07-10 23:41:03 +0300

    реализовано добавление Файла

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -13,7 +15,9 @@ public class ContactCreationTests extends TestBase {
   public void testNewContactCreation() throws Exception {
      app.goTo().homePage();
      Contacts before = app.contact().all();
-     ContactData contact = new ContactData().withName("ivan").withFirstname("gorelkin").withGroup("test1");
+     File photo = new File("src/test/resources/Ferma.png");
+     ContactData contact = new ContactData().withName("ivan").withFirstname("gorelkin")
+             .withPhoto(photo).withGroup("test1");
      app.contact().create(contact, true);
      assertThat(app.contact().count(), equalTo(before.size() + 1));
      Contacts after = app.contact().all();
