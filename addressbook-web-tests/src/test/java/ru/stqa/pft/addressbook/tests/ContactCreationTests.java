6b7f5022cad1264c59a90ae4b523bef598db05b0
GorelkinIA <48888453+GorelkinIA@users.noreply.github.com>
2019-07-11 21:37:23 +0300

    Попытка реализовать загрузку тестовых данных для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,9 +5,10 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -19,15 +20,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
    @DataProvider
-   public Iterator<Object[]> validContacts() {
+   public Iterator<Object[]> validContacts() throws IOException {
       List<Object[]> list = new ArrayList<Object[]>();
-      File photo = new File("src/test/resources/Ferma.png");
-      list.add(new Object[]{new ContactData().withName("ivan1").withFirstname("Gorelkin1")
-              .withMobileTelephone("887-551").withGroup("test1")});
-      list.add(new Object[]{new ContactData().withName("ivan2").withFirstname("Gorelkin2")
-              .withMobileTelephone("887-552").withGroup("test1")});
-      list.add(new Object[]{new ContactData().withName("ivan3").withFirstname("Gorelkin3")
-              .withMobileTelephone("887-553").withGroup("test1")});
+      BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contact.csv")));
+      String line = reader.readLine();
+      while (line != null) {
+         String[] split = line.split(";");
+         list.add(new Object[]{new ContactData().withName(split[0]).withFirstname(split[1])
+                 .withMobileTelephone(split[2]).withGroup(split[3])});
+         line = reader.readLine();
+      }
       return list.iterator();
    }
 
