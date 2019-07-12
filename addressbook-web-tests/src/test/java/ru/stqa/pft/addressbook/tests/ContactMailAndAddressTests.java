3a479d695f6d0c8a7769492afe8fe6fa7ee32470
GorelkinIA <48888453+GorelkinIA@users.noreply.github.com>
2019-07-12 21:50:12 +0300

    проверка информации о почте и адресе

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactMailAndAddressTests.java
@@ -0,0 +1,46 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ContactMailAndAddressTests extends TestBase{
+
+  @BeforeMethod
+  public void ensurePrecondition() {
+    app.goTo().homePage();
+    if (app.contact().all().size() == 0) {
+      app.contact().create(new ContactData().withName("ivan").withFirstname("gorelkin").withMobileTelephone("234-55")
+              .withHomePhone("363-22(15)").withGroup("test1").withAddress("15 Park, Russia 24/2").withMail("ivan@mail.ru").withMail2("mailjk@mail.com")
+              .withMail3("fjjdf@mail.ru"), true);
+    }
+  }
+
+  @Test
+  public void testContactTelephone() {
+    app.goTo().homePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFoEditForm = app.contact().infoFromEditForm(contact);
+
+    assertThat(contact.getAllMail(), equalTo(mergeMail(contactInfoFoEditForm)));
+    assertThat(contact.getAddress(), equalTo(megaAddress(contactInfoFoEditForm)));
+  }
+
+  private String mergeMail(ContactData contact) {
+    return Arrays.asList(contact.getMail(), contact.getMail2(), contact.getMail3())
+            .stream().filter((s) -> ! s.equals(""))
+            .collect(Collectors.joining("\n"));
+  }
+  private String megaAddress(ContactData contact) {
+    return Arrays.asList(contact.getAddress())
+            .stream().filter((s) -> ! s.equals(""))
+            .collect(Collectors.joining("\n"));
+  }
+}
+
