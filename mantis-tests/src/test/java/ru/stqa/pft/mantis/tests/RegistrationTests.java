6c0ef8143f9ff5fb6b8cb06d5c40cc776cca7607
GorelkinIA <48888453+GorelkinIA@users.noreply.github.com>
2019-07-18 16:18:00 +0300

    Сделан файл testng.xml для тестов мантиса.

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.testng.annotations.Test;
+
+public class RegistrationTests extends TestBase{
+
+  @Test
+  public void testRegistrationTest() {
+    app.registration().start("user1", "user1@localhost.localdomain");
+  }
+}
